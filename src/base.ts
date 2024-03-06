ae074b388e7b68dc50358986240cff3d81ff0cc4
mguleryuz <mguleryuz3@gmail.com>
2024-03-06 13:46:38 +0300

    refactor: simplified

--- src/base.ts
+++ src/base.ts
@@ -1,4 +1,4 @@
-export type ModuleTypes =
+export type ModuleType =
   | 'logicModule'
   | 'paymentProcessor'
   | 'authorizer'
--- src/index.ts
+++ src/index.ts
@@ -13,24 +13,3 @@ export type ModuleVersion<
   K extends ModuleKeys,
   V extends ModuleVersionKey,
 > = BaseData[K][V]
-
-export type Abi<
-  K extends ModuleKeys,
-  V extends ModuleVersionKey,
-> = ModuleVersion<K, V>['abi']
-
-export type MethodKey<K extends ModuleKeys, V extends ModuleVersionKey> = Abi<
-  K,
-  V
->[number]['name']
-
-export type AbiItem<
-  K extends ModuleKeys,
-  V extends ModuleVersionKey,
-  MK extends MethodKey<K, V> = MethodKey<K, V>,
-> = Extract<
-  Abi<K, V>[number],
-  {
-    name: MK
-  }
->
