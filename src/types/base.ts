200192cb30b44fe55463ac8df8919afed0449c7f
mguleryuz <mguleryuz3@gmail.com>
2024-05-13 20:07:34 +0300

    chore: base type tag

--- src/types/base.ts
+++ src/types/base.ts
@@ -1,6 +1,10 @@
 import { ModuleType } from './data'
 
-export type Tag = 'any' | 'decimals'
+export type Tag =
+  | 'any'
+  | 'decimals'
+  | `decimals:external:indirect:${string}`
+  | `decimals:internal:${'exact' | 'indirect'}:${string}`
 
 export type UserFacingModuleType = Exclude<
   ModuleType,
