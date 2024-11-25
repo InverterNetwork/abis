8e54b0532fca1e75e48a7eea583981c23f818b64
mguleryuz <mguleryuz3@gmail.com>
2024-11-25 11:20:43 +0700

    chore: add identifier to getmoduledata error

--- src/utils/get.ts
+++ src/utils/get.ts
@@ -5,6 +5,6 @@ export function getModuleData<N extends ModuleName>(name: N) {
   const moduleData = data.find(
     (module): module is GetModuleData<N> => module.name === name
   )
-  if (!moduleData) throw new Error('ModuleData Returned Undefined')
+  if (!moduleData) throw new Error(`ModuleData Returned Undefined for: ${name}`)
   return moduleData
 }
