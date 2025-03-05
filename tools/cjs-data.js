a931d6241a48104f870f0e3b2e9a53f905e9f9b2
mguleryuz <mguleryuz3@gmail.com>
2025-03-05 23:27:49 +0300

    chore(release): 0.3.28-alpha.3

---
+++ tools/cjs-data.js
@@ -0,0 +1,8 @@
+// prettier-ignore
+'use strict';
+// prettier-ignore
+Object.defineProperty(exports, '__esModule', { value: true });
+// prettier-ignore
+exports.data = void 0;
+// prettier-ignore
+exports.data = require('../esm/data.js').data;
